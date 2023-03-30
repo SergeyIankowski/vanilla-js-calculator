f84c77e0fcf8a54576d7f236ccaa093533400d09
SergeyIankowski <92864057+SergeyIankowski@users.noreply.github.com>
2023-03-30 07:20:22 +0300

    feat: allocate Invoker class from App class (#9)

---
+++ src/invoker/Invoker.js
@@ -0,0 +1,69 @@
+export default class Invoker {
+    constructor(calculator) {
+        this.calculator = calculator;
+
+        this.firstValue = null;
+        this.command = null;
+        this.secondValue = null;
+    }
+
+    addValue(value) {
+        const strValue = `${value}`;
+        if (this.firstValue === '0') {
+            this.firstValue = '';
+        }
+        if (this.command && this.firstValue) {
+            this.secondValue = this.secondValue ? `${this.secondValue}${strValue}` : strValue;
+            return;
+        }
+        this.firstValue = this.firstValue ? `${this.firstValue}${strValue}` : strValue;
+    }
+
+    addCommand(command) {
+        if (this.command && this.secondValue) this.calculate();
+        this.command = command;
+        this.reRenderBoard();
+    }
+
+    calculate() {
+        const Command = this.command;
+
+        if (!this.command) return;
+        if (this.command.needSecondValue() && this.secondValue) {
+            this.calculator.executeCommand(new Command(this.firstValue, this.secondValue));
+            this.refreshAppValues();
+        } else if (!this.command.needSecondValue()) {
+            this.calculator.executeCommand(new Command(this.firstValue));
+            this.refreshAppValues();
+        }
+        this.reRenderBoard();
+    }
+
+    clean() {
+        this.firstValue = null;
+        this.command = null;
+        this.secondValue = null;
+    }
+
+    pastDotToValue() {
+        if (this.secondValue && !`${this.secondValue}`.includes('.')) {
+            this.secondValue += '.';
+        } else if (!`${this.firstValue}`.includes('.')) {
+            this.firstValue += '.';
+        }
+    }
+
+    changeSign() {
+        if (this.secondValue) {
+            this.secondValue *= -1;
+        } else {
+            this.firstValue *= -1;
+        }
+    }
+
+    refreshAppValues() {
+        this.firstValue = this.calculator.value;
+        this.command = null;
+        this.secondValue = null;
+    }
+}
